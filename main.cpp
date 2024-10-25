1a27891b627142411e495b80415b329f0beece43
255601 <255601@edu.p.lodz.pl>
2024-10-25 09:04:21 +0200

    test2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int policz(int a, int b) {
+    return a+b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "W pierwszym branchu; 2+3=" << policz(3, 5) << std::endl;
     return 0;
 }
